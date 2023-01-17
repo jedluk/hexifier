6b2959b3cbfc785a70f9331bc8db589f188f5c67
Jedrzej Lukasiuk <jedrzej.lukasiuk@gmail.com>
2023-01-17 16:29:27 +0100

    cd script

--- src/DrawControl.tsx
+++ src/DrawControl.tsx
@@ -15,13 +15,19 @@ export default function DrawControl(props: DrawControlProps) {
   useControl<MapboxDraw>(
     () => new MapboxDraw({ ...props }),
     ({ map }: { map: MapRef }) => {
+      // @ts-expect-error
       map.on('draw.create', props.onCreate)
+      // @ts-expect-error
       map.on('draw.update', props.onUpdate)
+      // @ts-expect-error
       map.on('draw.delete', props.onDelete)
     },
     ({ map }: { map: MapRef }) => {
+      // @ts-expect-error
       map.off('draw.create', props.onCreate)
+      // @ts-expect-error
       map.off('draw.update', props.onUpdate)
+      // @ts-expect-error
       map.off('draw.delete', props.onDelete)
     },
     {
