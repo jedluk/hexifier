607031ba8b165fa95913daaeebbaa062c0d45e6f
Jedrzej Lukasiuk <jedrzej.lukasiuk@gmail.com>
2023-01-19 11:33:05 +0100

    move style to app bundle

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import 'maplibre-gl/dist/maplibre-gl.css'
+import '@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
