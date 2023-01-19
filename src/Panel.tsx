becf2bec144ce44787a6b33f058b9396f04c798f
Jedrzej Lukasiuk <jedrzej.lukasiuk@gmail.com>
2023-01-19 12:53:13 +0100

    typography

--- src/Panel.tsx
+++ src/Panel.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export function Panel() {
   return (
-    <div className="absolute z-10 top-5 left-5">
+    <div className="absolute z-10 top-5 left-5 bg-white rounded-md p-2">
       <h1 className="text-3xl font-bold underline">Hi from tailwind!</h1>
     </div>
   )
