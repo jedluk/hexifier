4f642e41443389d0c33995dc1205196ab04c315b
Jedrzej Lukasiuk <jedrzej.lukasiuk@gmail.com>
2023-01-19 12:31:09 +0100

    setup tailwind

---
+++ src/Panel.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export function Panel() {
+  return (
+    <div className="absolute z-10 top-5 left-5">
+      <h1 className="text-3xl font-bold underline">Hi from tailwind!</h1>
+    </div>
+  )
+}
