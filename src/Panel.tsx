ddc0d05cb06f6c64bf3cb30143aa768e89b62a91
Jedrzej Lukasiuk <jedrzej.lukasiuk@gmail.com>
2023-01-19 15:05:56 +0100

    panel PoC

--- src/Panel.tsx
+++ src/Panel.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
-import { Logo } from './components/svg'
+import { Area, Logo } from './components/svg'
 import { Upload } from './Upload'
 
 export function Panel() {
   return (
     <div className="absolute z-10 top-2 left-2 w-60 bg-white rounded-md p-2">
       <Logo height={24} />
-      <h2>Draw polygon</h2>
-      <h3>or</h3>
+      <h2 className="my-5 flex">
+        <Area />
+        &nbsp;
+        <span>draw polygon or</span>
+      </h2>
       <Upload />
     </div>
   )
--- src/Upload.tsx
+++ src/Upload.tsx
@@ -3,7 +3,7 @@ import { Upload as UploadIcon } from './components/svg'
 
 export function Upload() {
   return (
-    <div className="h-20 border border-dashed border-gray-200 hover:border-gray-500 rounded-sm flex flex-col justify-center items-center">
+    <div className="h-20 border border-dashed border-gray-200 hover:border-gray-500 rounded-sm flex flex-col justify-center items-center cursor-pointer">
       <UploadIcon />
       <span>Upload geoJSON</span>
     </div>
