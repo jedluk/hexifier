d04522061b5fb54898d96fb238f4ae9fe76a7405
Jedrzej Lukasiuk <jedrzej.lukasiuk@gmail.com>
2023-01-20 10:10:01 +0100

    not implemented info

--- src/Panel.tsx
+++ src/Panel.tsx
@@ -16,7 +16,7 @@ export function Panel(props: PanelProps) {
   const { polygons, onZoomToPolygon } = props
 
   return (
-    <div className="absolute z-10 top-2 left-2 w-96 h-96 flex flex-col bg-white rounded-md p-2 shadow-lg break-words">
+    <div className="absolute z-10 top-2 left-2 w-96 h-96 flex flex-col bg-white rounded-xl p-2 shadow-lg break-words">
       <Logo width={256} height={50} />
       <RenderWhen condition={isEmpty(polygons)}>
         <h2 className="my-5 font-bold text-center uppercase underline decoration-wavy decoration-orange-300">
