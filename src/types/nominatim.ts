ac0172f8052c62917f2d3df075cd77c6f7a348b8
Jedrzej Lukasiuk <jedrzej.lukasiuk@gmail.com>
2023-01-30 18:03:10 +0100

    network infrastructure

---
+++ src/types/nominatim.ts
@@ -0,0 +1,35 @@
+/* eslint-disable @typescript-eslint/no-namespace */
+export namespace Nominatim {
+  interface Geojson {
+    type: 'Polygon'
+    coordinates: [number, number][][]
+  }
+
+  export interface AddressDetails {
+    house_number?: string
+    road?: string
+    neighbourhood: string
+    suburb?: string
+    borough: string
+    city: string
+    state: string
+    postcode?: string
+    country: string
+    country_code: string
+  }
+
+  export interface OSMElement {
+    place_id: number
+    licence: string
+    osm_id: number
+    osm_type: 'node' | 'way' | 'relation'
+    boundingbox: [string, string, string, string]
+    display_name: string
+    lat: string
+    lon: string
+    address: AddressDetails
+    importance: number
+    addresstype: string
+    geojson: Geojson
+  }
+}
