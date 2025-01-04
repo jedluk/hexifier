70b3bef86fb0f022e62fb40d9359ebc0fd04e027
Jedrzej Lukasiuk <jedrzej.lukasiuk@gmail.com>
2025-01-04 12:45:28 +0100

    Fix dumping map on mobile devices

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -14,7 +14,7 @@ const SMALL_SCREEN_MAP_PADDING = {
   bottom: 10,
   left: 10,
   right: 10,
-  top: 100
+  top: 50
 } as const
 
 const MEDIUM_SCREEN_MAP_PADDING = {
@@ -32,12 +32,20 @@ export function isMobileScreen(): boolean {
   return matchMedia('(max-width: 768px)').matches
 }
 
-export function getSidebarRatio(): number {
+export function getSidebarRatio(isPanelOpen: boolean): number {
+  if (isMobileScreen()) {
+    return 0
+  }
   return (
-    (isMobileScreen()
-      ? MEDIUM_SCREEN_MAP_PADDING.left
-      : MEDIUM_SCREEN_MAP_PADDING.left) / window.innerWidth
+    (Number(isPanelOpen) / window.innerWidth) * MEDIUM_SCREEN_MAP_PADDING.left
   )
+}
+
+export function getTopBarRatio(): number {
+  if (!isMobileScreen()) {
+    return 0
+  }
+  return SMALL_SCREEN_MAP_PADDING.top / window.innerHeight
 }
 
 export function getMapPadding(): MapPadding {
